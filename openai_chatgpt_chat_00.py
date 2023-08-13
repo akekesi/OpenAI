0a81416d41e563e9f86b31f61a04ad4769d8bf6a
KEKESATI <KEKESATI@schaeffler.com>
2023-08-13 11:12:41 +0200

    created api.key and chat_gpt_chat_00.py

---
+++ openai_chatgpt_chat_00.py
@@ -0,0 +1,63 @@
+"""
+sources:
+https://www.youtube.com/watch?v=R3mo_OJO5pM
+"""
+import openai
+
+
+class ChatGPT:
+	def __init__(self, api_key: str, role: str):
+		openai.api_key = api_key
+		self.dialog = [
+			{
+				"role": "system",
+				"content": role
+			}
+		]
+
+	def fragen(self, question):
+		self.dialog.append(
+			{
+				"role": "user",
+				"content": question
+			}
+		)
+		response = openai.ChatCompletion.create(
+			model="gpt-3.5-turbo",
+			messages=self.dialog
+		)
+		answer = response.choices[0].message.content
+		self.dialog.append(
+			{
+				"role": "assistant",
+				"content": answer
+			}
+		)
+		return answer
+
+
+if __name__ == "__main__":
+	# arguments to run chat
+	path_api_key = "api.key"						# <-- api.key file
+	path_save_text = "chat_gpt_pirate_BJ.txt"		# <-- txt file to save chat
+	role = "Be a pirate who likes Ben & Jerry's."	# <-- role of ChatGPT
+
+	# create ChatGPT
+	with open(path_api_key, "r") as api_key_open:
+		api_key = api_key_open.read()
+	chatgpt = ChatGPT(
+		api_key=api_key,
+		role=role
+	)
+
+	# run chat, terminate it with "EXIT"
+	while True:
+		question = input("\n> ")
+		if question == "EXIT":
+			break
+		answer = chatgpt.fragen(question=question)
+		print(answer)
+
+		with open(path_save_text, "a") as f:
+			f.write(f"> {question}\n")
+			f.write(f"> {answer}\n")
