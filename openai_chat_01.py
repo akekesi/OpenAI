17b78d85e278a73f0d279fabdbe4d6aa9d1e0e15
Kekesi, Attila  IT/HZA-TF <KEKESATI@schaeffler.com>
2023-08-17 03:39:38 +0200

    used PEP8 (tab-->space, arguments)

--- openai_chat_01.py
+++ openai_chat_01.py
@@ -7,38 +7,38 @@ import openai
 
 
 class ChatTextDavinci003:
-	def __init__(self, api_key: str):
-		openai.api_key = api_key
+    def __init__(self, api_key: str):
+        openai.api_key = api_key
 
-	def question(self, question: str):
-		response = openai.Completion.create(
-			model="text-davinci-003",
-			prompt=question
-		)
-		answer = response.choices[0].text
-		return answer
+    def question(self, question: str):
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=question
+        )
+        answer = response.choices[0].text
+        return answer
 
 
 if __name__ == "__main__":
-	# arguments
-	path_api_key = "api.key"			# <-- api.key file, paste your api key there
-	path_save_chat = "chat_davinci.txt"	# <-- txt file to save chat
-	with open(path_api_key, "r") as api_key_open:
-		api_key = api_key_open.read()
+    # arguments
+    path_api_key = "api.key"			# <-- api.key file, paste your api key there
+    path_save_chat = "chat_davinci.txt"	# <-- txt file to save chat
+    with open(path_api_key, "r") as api_key_open:
+        api_key = api_key_open.read()
 
-	# create chat object
-	chatgpt = ChatTextDavinci003(
-		api_key=api_key
-	)
+    # create chat object
+    chatgpt = ChatTextDavinci003(
+        api_key=api_key
+    )
 
-	# run and save chat, terminate it with "EXIT"
-	while True:
-		question = input("\n> ")
-		if question == "EXIT":
-			break
-		answer = chatgpt.question(question=question)
-		print(answer)
+    # run and save chat, terminate it with "EXIT"
+    while True:
+        question = input("\n> ")
+        if question == "EXIT":
+            break
+        answer = chatgpt.question(question=question)
+        print(answer)
 
-		with open(path_save_chat, "a") as f:
-			f.write(f"> {question}\n")
-			f.write(f"> {answer}\n")
+        with open(path_save_chat, "a") as f:
+            f.write(f"> {question}\n")
+            f.write(f"> {answer}\n")
