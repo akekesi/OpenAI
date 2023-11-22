828e5918de6c24385743474167da307336a5c7ca
akekesi <kekesi.att@gmail.com>
2023-11-22 17:54:34 +0100

    refactored exe/create_exe.sh

--- exe/create_exe.sh
+++ exe/create_exe.sh
@@ -4,24 +4,24 @@
 # - paths are relative to the main folder of the repository
 
 # variables
-VENV="venv_tmp_exe"
-NAME="app_openai_main"
+venv="venv_tmp_exe"
+name="app_openai_main"
 
-# check existence of exe/$NAME
-if [ -d "exe/$NAME" ]; then
-echo "Error: "exe/$NAME" already exist."
-exit 1
+# check existence of exe/$name
+if [ -d "exe/$name" ]; then
+    echo "Error: "exe/$name" already exist."
+    exit 1
 fi
 
-# check existence of $VENV
-if [ -d $VENV ]; then
-echo "Error: $VENV already exist."
-exit 1
+# check existence of $venv
+if [ -d $venv ]; then
+    echo "Error: $venv already exist."
+    exit 1
 fi
 
 # venv: create and activate
-python -m venv $VENV
-. "$VENV/Scripts/activate"
+python -m venv $venv
+. "$venv/Scripts/activate"
 
 # pip install
 pip install -r "requirements.txt"
@@ -31,17 +31,17 @@ pip install pywin32
 # pyinstaller
 pyinstaller --noconfirm --onedir --windowed \
     --icon "img/image_default.ico" \
-    --add-data "$VENV/Lib/site-packages/customtkinter;customtkinter/" \
+    --add-data "$venv/Lib/site-packages/customtkinter;customtkinter/" \
     --distpath "exe" \
-    "src/$NAME.py"
+    "src/$name.py"
 
 # copy and delete folders
 rm -rf "build"
-rm -rf "$NAME.spec"
-cp -r "doc" "exe/$NAME/doc"
-cp -r "img" "exe/$NAME/img"
-cp "api.key" "exe/$NAME/api.key"
+rm -rf "$name.spec"
+cp -r "doc" "exe/$name/doc"
+cp -r "img" "exe/$name/img"
+cp "api.key" "exe/$name/api.key"
 
 # venv: deactivate and delete
 deactivate
-rm -rf "$VENV"
+rm -rf "$venv"
